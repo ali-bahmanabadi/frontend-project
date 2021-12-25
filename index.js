c5728338271d777c685e5823b0eaafacda965524
ali bahmanabadi <alibahmanabadi00@gmail.com>
2021-12-25 12:50:22 +0330

    finish project 1

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const emojiList = ['🤗', '🍀', '(⚈ ̫ ⚈)', '😀', '😁', '😅', '😇', '😉'];
-const p = document.querySelector('p');
-p.addEventListener('click', () => {
-  p.innerText = emojiList[Math.floor((Math.random() * 100) % emojiList.length)];
-});
+const backBtn = document.querySelector(".back")
+const menu = document.querySelector('.menu')
+const followBtn = document.querySelector('.follow-btn')
+const messageBtn = document.querySelector('.message-btn')
+const suggestBtn = document.querySelector('.suggest-btn')
+
+
+backBtn.addEventListener('click', () => {
+  console.log("clicked back button")
+})
+
+menu.addEventListener('click', () => {
+  console.log("clicked menu button")
+})
+
+followBtn.addEventListener('click', () => {
+  console.log("clicked follow button")
+})
+
+messageBtn.addEventListener('click', () => {
+  console.log('clicked message button')
+})
+
+suggestBtn.addEventListener('click', () => {
+  console.log('clicked suggest button')
+})
